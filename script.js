077159ef4b92b513d74ba64e3e4ddffebae65484
Designer <visualdesign3r@gmail.com>
2020-01-25 23:38:47 -0800

    new files

---
+++ script.js
@@ -0,0 +1,9 @@
+function openNav() {
+    document.getElementById("myNav").style.height = "100%";
+    document.getElementById("myNav2").style.height = "100%";
+  }
+  
+  function closeNav() {
+    document.getElementById("myNav").style.height = "0%";
+    document.getElementById("myNav2").style.height = "0%";
+  }
